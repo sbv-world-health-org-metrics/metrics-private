90b93f29d8e370aa3522fa2404720490a06b8acf
Diego Arostegui <darostegui@github.com>
2023-05-01 11:10:40 +0200

    Update users.py

--- users.py
+++ users.py
@@ -20,7 +20,7 @@ query UserInfo($UserName: String!){
     twitterUsername
     websiteUrl
     url
-    organizationVerifiedDomainEmails(login: world-health-organization)
+    organizationVerifiedDomainEmails(login: "world-health-organization")
   }
 }
 '''
